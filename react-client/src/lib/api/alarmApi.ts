184799ae8265d5aa6d2bf1be3fb3d6258ed33755
Newst Fillantrophy <fillantrophy@gmail.com>
2024-12-02 20:17:21 +0800

    move db n delete deprecated code

--- react-client/src/lib/api/alarmApi.ts
+++ react-client/src/lib/api/alarmApi.ts
@@ -3,6 +3,6 @@ import apiClient from ".";
 const alarmApi = {
     set: (alarm: string) => apiClient.post("/", { type: "alarm", command: "set", time: alarm }),
     unset: (alarm: string) => apiClient.post("/", { type: "alarm", command: "unset", time: alarm }),
-}
+} // e.g. 07:00 or delete row
 
 export default alarmApi;
--- server/build.sh
+++
@@ -1,5 +0,0 @@
-# remove the container if it exists
-docker rm -f ilight-server
-
-docker build -t ilight-server .
-docker run -it -p 3001:3001 --env-file .env --name ilight-server ilight-server
